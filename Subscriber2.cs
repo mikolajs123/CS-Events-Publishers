8195b3f5dbf972804a26623338b07d8e771e118d
mikolajs123 <53497782+mikolajs123@users.noreply.github.com>
2021-05-25 10:53:34 +0200

    Update Subscriber2.cs

--- Subscriber2.cs
+++ Subscriber2.cs
@@ -4,10 +4,10 @@ namespace EventsAndPublishers
 {
 	public class Subscriber2
 	{
-        // Słucham czy publisher wysłał powiadomienie do mnie z argmuentem
-        public void OnArgumented(object source, MyEventArgs args)
-        {
-            Console.WriteLine($"[Subscriber 2]: Właśnie otrzymałem powiadomienie od Publishera z argumentem: {args.Message}");
-        }
+		// Słucham czy publisher wysłał powiadomienie do mnie z argmuentem
+		public void OnArgumented(object source, MyEventArgs args)
+		{
+		    Console.WriteLine($"[Subscriber 2]: Właśnie otrzymałem powiadomienie od Publishera z argumentem: {args.Message}");
+		}
 	}
 }
