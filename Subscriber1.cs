16015df5183b99035ee259d01b75eec8326ab727
mikolajs123 <53497782+mikolajs123@users.noreply.github.com>
2021-05-25 10:53:15 +0200

    Update Subscriber1.cs

--- Subscriber1.cs
+++ Subscriber1.cs
@@ -4,8 +4,8 @@ namespace EventsAndPublishers
 {
 	public class Subscriber1
 	{
-        // Słucham czy publisher wysłał powiadomienie do mnie bez argmuentu
-        public void OnNonArgumented(object source, EventArgs args)
+        	// Słucham czy publisher wysłał powiadomienie do mnie bez argmuentu
+        	public void OnNonArgumented(object source, EventArgs args)
 		{
 			Console.WriteLine($"[Subscriber 1]: Właśnie otrzymałem powiadomienie od Publishera bez argumentu");
 		}
